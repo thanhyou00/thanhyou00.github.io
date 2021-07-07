bd0490a93ed75c25c345c891eb92c98f75d84e8d
= <=>
2021-07-07 22:11:10 +0700

    thanhyou00

--- Project_One/mypro.js
+++ Project_One/mypro.js
@@ -2,7 +2,7 @@
 
 var pass = prompt('Please enter a password1')
 
-if(pass=='thanhyou00') {
+if(pass=='thanhyou01') {
     alert('You have successfully accessed !')
 } else {
     alert('Access is denied !')
